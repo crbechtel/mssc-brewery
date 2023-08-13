6a3201b3672ede5a686750d18bd8f317a387c3fc
cliffordbechtel <crbechtel@gmail.com>
2023-08-13 19:38:51 -0400

    mapstruct

--- src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
@@ -8,6 +8,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 /**
@@ -30,4 +31,7 @@ public class BeerDto {
 
     @Positive
     private Long upc;
+
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastUpdatedDate;
 }
